e7965655272e6a7c4c208ea3c2c6e770f9f75b25
Fabricio <fabricio.a.s@hotmail.com>
2019-02-06 10:05:05 -0300

    criptografia

--- 26-criptografia.php
+++ 26-criptografia.php
@@ -5,3 +5,18 @@ $senha = "123456";
 $novasenha = base64_encode($senha);
 echo "base64: ".$novasenha."<br>";
 echo "Sua senha é: ". base64_decode($novasenha);
+
+echo "<hr>";
+
+echo "MD5: ".md5($senha);
+echo "<br>";
+echo "Sha1: ". sha1($senha);
+
+echo "<hr>";
+
+$options = [
+  'cost' => 10;
+];
+$senhaSegura = password_hash($senha, PASSWORD_DEFAULT, $options);
+
+echo $senhaSegura;
